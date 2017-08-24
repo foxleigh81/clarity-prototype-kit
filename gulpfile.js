1aa43b07b150369116bd28a1b4138563d4725bf3
Alex Foxleigh <alex@volcaniccreations.com>
2017-08-24 10:16:12 +0100

    design idea - two column

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,87 @@
-var gulp        = require('gulp'),
-    gulpif      = require('gulp-if'),
-    gutil       = require('gulp-util'),
-    chalk       = require('chalk'),
-    argv        = require('yargs').argv,
-    fs          = require('fs'),
-    browserSync = require('browser-sync'),
-    addsrc      = require('gulp-add-src'),
-    concat      = require('gulp-concat'),
-    imagemin    = require('gulp-imagemin'),
-    pngquant    = require('imagemin-pngquant'),
-    plumber     = require('gulp-plumber'),
-    notifier    = require('node-notifier'),
-    cssnano     = require('gulp-cssnano'),
-    jsonlint    = require('gulp-jsonlint'),
-    del         = require('del'),
-    standard    = require('gulp-standard'),
-    stylus      = require('gulp-stylus'),
-    jeet        = require('jeet'),
-    rupture     = require('rupture'),
-    csso        = require('gulp-csso'),
-    extReplace  = require('gulp-ext-replace'),
-    runSequence = require('run-sequence'),
-    uglify      = require('gulp-uglify'),
-    cmq         = require('gulp-merge-media-queries'),
-    nunjucks    = require('gulp-nunjucks-html'),
-    path        = require('path'),
-    rename      = require('gulp-rename'),
-    data        = require('gulp-data'),
-    webpack     = require('webpack-stream'),
-    WebpackDevServer = require('webpack-dev-server'),
-    reload      = browserSync.reload;
-
+var gulp = require('gulp')
+var chalk = require('chalk')
+var argv = require('yargs').argv
+var fs = require('fs')
+var browserSync = require('browser-sync')
+var concat = require('gulp-concat')
+var imagemin = require('gulp-imagemin')
+var pngquant = require('imagemin-pngquant')
+var plumber = require('gulp-plumber')
+var notifier = require('node-notifier')
+var cssnano = require('gulp-cssnano')
+var jsonlint = require('gulp-jsonlint')
+var del = require('del')
+var standard = require('gulp-standard')
+var stylus = require('gulp-stylus')
+var jeet = require('jeet')
+var rupture = require('rupture')
+var csso = require('gulp-csso')
+var extReplace = require('gulp-ext-replace')
+var runSequence = require('run-sequence')
+var uglify = require('gulp-uglify')
+var cmq = require('gulp-merge-media-queries')
+var nunjucks = require('gulp-nunjucks-html')
+var path = require('path')
+var rename = require('gulp-rename')
+var data = require('gulp-data')
+var reload = browserSync.reload
 
 // watch files for changes and reload
-gulp.task('serve', function() {
-  'use strict';
+gulp.task('serve', function () {
+  'use strict'
   browserSync.init({
-      server: {
-          baseDir: './dist'
-      },
+    server: {
+      baseDir: './dist'
+    },
       // Change the default port
-      port: 3000,
+    port: 3000,
       // All open instances of the site will reload if the server is restarted
-      reloadOnRestart: true,
+    reloadOnRestart: true,
       // // Don't show any notifications in the browser.
-      notify: false,
+    notify: false,
       // // Sync actions between devices
-      ghostMode: {
-        clicks: true,
-        forms: true,
-        scroll: false
-      }
-  });
+    ghostMode: {
+      clicks: true,
+      forms: true,
+      scroll: false
+    }
+  })
   // Perform the site init
-  runSequence('build');
+  runSequence('build')
 
   // Compile Standard JS
-  gulp.watch('src/**/*.js', ['scripts'], reload);
+  gulp.watch('src/**/*.js', ['scripts'], reload)
 
   // Compile Stylus
-  gulp.watch('src/**/*.styl', ['styles']);
+  gulp.watch('src/**/*.styl', ['styles'])
 
   // Compile Standard JS
-  gulp.watch('src/images', ['images'], reload);
+  gulp.watch('src/images', ['images'], reload)
 
   // Compile HTML and JSON
-  gulp.watch(['src/components/**/*.njk', 'src/templates/**/*.njk', 'src/pages/**/*.njk', 'src/model/**/*.json'], ['processHTML'], reload);
+  gulp.watch(['src/components/**/*.njk', 'src/templates/**/*.njk', 'src/pages/**/*.njk', 'src/model/**/*.json'], ['processHTML'], reload)
 
   // Show success message
   console.log(chalk.green('✔ Server started!'))
-});
+})
 
-var errors = 0,
-    supportedBrowsers = [
-    'last 2 versions',
-    'safari >= 8',
-    'ie >= 10',
-    'ff >= 20',
-    'ios 6',
-    'android 4'
-],
+var errors = 0
+var supportedBrowsers = [
+  'last 2 versions',
+  'safari >= 8',
+  'ie >= 10',
+  'ff >= 20',
+  'ios 6',
+  'android 4'
+]
 
-onError = function (err) {
-  'use strict';
+var onError = function (err) {
   console.log(chalk.red('✘ Build failed!'))
-  notifier.notify({ title: 'Build', message: 'Failed'});
-  console.log(err);
-  errors = errors+1
-  this.emit('end');
-};
+  notifier.notify({ title: 'Build', message: 'Failed' })
+  console.log(err)
+  errors = errors + 1
+  this.emit('end')
+}
 
 // Combine styles
 /* styles task */
@@ -194,16 +187,16 @@ gulp.task('static', function (done) {
 })
 
 // Move json
-gulp.task('model', function() {
-  'use strict';
+gulp.task('model', function () {
+  'use strict'
   return gulp.src([
-    'src/model/**/*',
-    ])
+    'src/model/**/*'
+  ])
     .pipe(plumber(
       { errorHandler: onError }
     ))
     .pipe(gulp.dest('dist/model'))
-});
+})
 
 /* Scripts task */
 gulp.task('scripts', function () {
@@ -235,87 +228,83 @@ gulp.task('scripts', function () {
   .pipe(browserSync.stream())
 })
 
-//Make sure the json data is correctly formed
-gulp.task('testModel', function() {
-  'use strict';
+// Make sure the json data is correctly formed
+gulp.task('testModel', function () {
+  'use strict'
   return gulp.src('./src/model/**/*.json')
     .pipe(jsonlint())
-    .pipe(jsonlint.reporter());
-});
+    .pipe(jsonlint.reporter())
+})
 
 // Move robots.txt to site root
-gulp.task('robots', function() {
-  'use strict';
+gulp.task('robots', function () {
+  'use strict'
   return gulp.src('robots.txt')
     .pipe(gulp.dest('dist'))
-});
+})
 
-gulp.task('processHTML', function() {
-  'use strict';
+gulp.task('processHTML', function () {
+  'use strict'
   return gulp.src('src/pages/*.njk')
     .pipe(plumber(
       { errorHandler: onError }
     ))
-    .pipe(data(function(file) {
-      return JSON.parse(fs.readFileSync('src/model/' + path.basename(file.path, '.njk') + '.json'));
+    .pipe(data(function (file) {
+      return JSON.parse(fs.readFileSync('src/model/' + path.basename(file.path, '.njk') + '.json'))
     }))
-    .pipe(data(function() {
-      return JSON.parse(fs.readFileSync('src/model/globals.json'));
+    .pipe(data(function () {
+      return JSON.parse(fs.readFileSync('src/model/globals.json'))
     }))
     .pipe(nunjucks({
       searchPaths: ['src/pages', 'src/components', 'src/templates']
     }))
     .pipe(extReplace('.html'))
     .pipe(gulp.dest('dist'))
-    .pipe(reload({stream:true}))
-});
+    .pipe(reload({stream: true}))
+})
 
-var prodBuild = ['clean', 'styles','vendors','static','scripts','images','processHTML','robots', 'notify'],
-    devBuild = ['clean', 'styles','vendors','static','scripts','images','processHTML','notify'],
-    buildTasks = argv.prod ? prodBuild : devBuild;
+var prodBuild = ['clean', 'styles', 'vendors', 'static', 'scripts', 'images', 'processHTML', 'robots', 'notify']
+var devBuild = ['clean', 'styles', 'vendors', 'static', 'scripts', 'images', 'processHTML', 'notify']
+var buildTasks = argv.prod ? prodBuild : devBuild
 
 // Perform Basic Build (note, don't call directly, use build:dev or build)
 gulp.task('build', function (callback) {
-  'use strict';
+  'use strict'
   runSequence.apply(
     null,
     buildTasks,
     callback
-  );
-});
+  )
+})
 
-
-gulp.task('notify', function() {
-  'use strict'
+gulp.task('notify', function () {
   console.log(chalk.green('✔ Build complete!'))
-  notifier.notify({ title: 'Build', message: 'Completed'});
+  notifier.notify({ title: 'Build', message: 'Completed' })
 })
 
 // Compress and minify images to reduce their file size
-gulp.task('images', function() {
-  'use strict';
-  var imgSrc = 'src/images/**/*',
-      imgDst = 'dist/static/images';
+gulp.task('images', function () {
+  var imgSrc = 'src/images/**/*'
+  var imgDst = 'dist/static/images'
 
   return gulp.src(imgSrc)
     .pipe(plumber())
     .pipe(imagemin({
-         progressive: true,
-         svgoPlugins: [{removeViewBox: false}],
-         use: [pngquant()]
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
     }))
     .pipe(gulp.dest(imgDst))
-});
+})
 
 // delete generated styles
 gulp.task('clean-styles', function (done) {
   // You can use multiple globbing patterns as you would with `gulp.src`
-  del('assets/css')
+  del('dist/static/css')
   done()
 })
 
 // Deletes contents of dist folder
-gulp.task('clean', function(cb) {
-    'use strict';
-    return del(['dist/**/*.*'], cb)
-});
+gulp.task('clean', function (cb) {
+  return del(['dist/**/*.*'], cb)
+})
