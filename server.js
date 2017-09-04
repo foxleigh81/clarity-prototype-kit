fbb8d9006f94119568f2972d5bc87c72a6c4c439
Alex Foxleigh <alex@volcaniccreations.com>
2017-09-04 12:34:01 +0100

    modified basic auth to add challenge

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ var basicAuth = require('express-basic-auth')
 
 var port = process.env.PORT || 5000
 
-function isHeroku () {
-  return process.env.NODE && ~process.env.NODE.indexOf('heroku') ? true : false
-}
-
-if (isHeroku()) {
-  app.use(basicAuth({
-    users: { 'admin': process.env.AUTH_KEY }
-  }))
-}
+app.use(basicAuth({
+  users: { 'admin': process.env.AUTH_KEY },
+  challenge: true
+}))
 
 app.use(express.static(__dirname + '/dist'))
 
