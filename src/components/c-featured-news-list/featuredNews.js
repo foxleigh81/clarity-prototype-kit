a689c85c33499dabbb6e5d7e4bacd673bc23f139
Alex Foxleigh <alex@volcaniccreations.com>
2017-10-05 17:02:44 +0100

    Set up styling to accomodate 1, 2 or 3 featured articles

--- src/components/c-featured-news-list/featuredNews.js
+++ src/components/c-featured-news-list/featuredNews.js
@@ -5,7 +5,7 @@
     var element = $(this)
     var articles = element.find('.c-article-item')
     for (var i = 0; i < articles.length; i++) {
-      $(articles[i]).addClass('u-article-' + (i + 1) + '-of-' + articles.length)
+      $(articles[i]).addClass('article-' + (i + 1) + '-of-' + articles.length)
     }
     return element
   }
